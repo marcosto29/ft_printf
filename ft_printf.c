990725cd8d970444d18391f47683510ff2d62003
Marcos <marcos29diciembre@gmail.com>
2025-04-23 14:27:56 +0200

    primera aproximacion

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,28 @@
 /*   By: matoledo <matoledo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 12:56:53 by matoledo          #+#    #+#             */
-/*   Updated: 2025/04/22 14:28:28 by matoledo         ###   ########.fr       */
+/*   Updated: 2025/04/23 13:45:53 by matoledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	size_calculator(char const *input_text, ...)
+static void	print_selector(char type, va_list )
+{
+
+	//cspdiuxX %
+	if (type == "c")
+		write(1, 1);
+	if (type == "s")
+	if (type == "p")
+	if (type == "d")
+	if (type == "i")
+	if (type == "u")
+	if (type == "x")
+	if (type == "X")
+	if (type == 37)
+}
+static int	size_calculator(char const *input_text, ...)
 {
 	int	size;
 
@@ -24,10 +39,19 @@ int	ft_printf(char const *input_text, ...)
 {
 	va_list	arguments;
 	int	size_return;
+	void *variable;
 
 	va_start(arguments, input_text);
-	while (*input_text && *input_text != 37)
+	while (*input_text)
+	{
+		if (*input_text == 37)
+		{
+
+			print_selector(*input_text++);
+		}
 		write(1, input_text++, 1);
+	}
+
 	va_arg(arguments, int);
 	return (0);
 }
@@ -37,5 +61,5 @@ int	main(void)
 	int	a;
 
 	a = 42;
-	ft_printf("hola");
+	ft_printf("hola %c", 3);
 }
